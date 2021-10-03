951eccd75e05f3ace53740ea5844de7ef33ded6c
olivercrush <olivier.beguinti@gmail.com>
2021-10-03 11:26:55 +0400

    created a display map method and a focus point property

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -4,23 +4,45 @@ using UnityEngine;
 
 public class Map : MonoBehaviour
 {
+    public int _mapViewSize = 10;
+    private int _mapViewFocus;
+
     private int[,] _mapData;
+    private GameObject[,] _mapCells;
 
     void Start()
     {
-        _mapData = FileUtils.ReadLevelFromFile("000");
-
-        for (int y = 0; y < _mapData.GetLength(0); y++)
-        {
-            for (int x = 0; x < _mapData.GetLength(1); x++)
-            {
-                Instantiate(ColorDebugPrefabFactory.GetMapCell(_mapData[y, x]), new Vector3(transform.position.x + x, transform.position.y + (_mapData.GetLength(0) - y - 1), 1), Quaternion.identity, transform);
-            }
-        }
+        LoadLevel("000");
     }
 
     void Update()
     {
         
+    }
+
+    private void LoadLevel(string name)
+    {
+        _mapData = FileUtils.ReadLevelFromFile(name);
+        _mapCells = new GameObject[_mapData.GetLength(0), _mapData.GetLength(1)];
+
+        DisplayMap();
+    }
+
+    private void DisplayMap()
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        for (int y = 0; y < _mapData.GetLength(0); y++)
+        {
+            for (int x = 0; x < _mapData.GetLength(1); x++)
+            {
+                GameObject cell = Instantiate(ColorDebugPrefabFactory.GetMapCell(_mapData[y, x]), new Vector3(transform.position.x + x, transform.position.y + (_mapData.GetLength(0) - y - 1), 1), Quaternion.identity, transform);
+                cell.name = x + ":" + y + " - " + _mapData[y, x];
+                _mapCells[y, x] = cell;
+            }
+        }
     }
 }
