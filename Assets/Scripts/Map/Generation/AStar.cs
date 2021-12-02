e5365e012316f4e7b424779745a8a2ac19bf370b
olivercrush <olivier.beguinti@gmail.com>
2021-12-02 22:42:15 +0400

    cleaned RiverGeneration and added AStar class

---
+++ Assets/Scripts/Map/Generation/AStar.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class AStar {
+    public static List<Vector2> GeneratePath(int[,] map, float[,] noise, Vector2 start, Vector2 goal) {
+
+        AStarNode goalNode = null;
+
+        // Create open and closed lists
+        List<AStarNode> open = new List<AStarNode>();
+        List<AStarNode> closed = new List<AStarNode>();
+
+        // Put node_start in the open list
+        open.Add(new AStarNode(null, start, goal, 0));
+
+        // While the open list is not empty
+        while (open.Count > 0) {
+            // Take the node with the lowest f from the open list
+            AStarNode current = GetLowestF(open);
+            open.Remove(current);
+
+            DebugUtils.DumpString("current : " + current.GetPosition().ToString() + " / goal : " + goal.ToString());
+
+            // If the current node is the goal, we found the solution
+            if (current.GetPosition() == goal) {
+                goalNode = current;
+                break;
+            }
+
+            // We generate all neighbours from the current node
+            foreach (Vector2 neighbourPos in GetNeighbours(map, current.GetPosition())) {
+
+                DebugUtils.DumpString("checking neighbour : " + neighbourPos.ToString());
+
+                // We create the neighbour node and set its f
+                float cellNoise = noise[(int)neighbourPos.y, (int)neighbourPos.x];
+
+                float mapDivider;
+                switch (map[(int)neighbourPos.y, (int)neighbourPos.x]) {
+                    case 2:
+                        mapDivider = 0.0000000000000000001f;
+                        break;
+
+                    case 3:
+                        mapDivider = 100000000000000000000f;
+                        break;
+
+                    default:
+                        mapDivider = 1f;
+                        break;
+                }
+
+                float neighbourCost = current.GetG() + cellNoise / mapDivider;
+
+
+                if (IsInList(open, neighbourPos)) {
+                    AStarNode tmp = GetNodeFromList(open, neighbourPos);
+                    DebugUtils.DumpString("neighbour is in open list");
+                    if (tmp.GetG() <= neighbourCost) continue;
+                    DebugUtils.DumpString("with higer g value");
+                }
+                else if (IsInList(closed, neighbourPos)) {
+                    AStarNode tmp = GetNodeFromList(closed, neighbourPos);
+                    DebugUtils.DumpString("neighbour is in closed list");
+                    if (tmp.GetG() <= neighbourCost) continue;
+                    DebugUtils.DumpString("with higer g value, sending it to open list");
+                    open.Add(tmp);
+                    RemoveFromList(closed, tmp);
+                }
+                else {
+                    DebugUtils.DumpString("neighbour is in no lists, sending it to open list");
+                    AStarNode neighbourNode = new AStarNode(current, neighbourPos, goal, neighbourCost);
+                    open.Add(neighbourNode);
+                }
+            }
+
+            closed.Add(current);
+        }
+
+        List<Vector2> path = new List<Vector2>();
+        AStarNode buffer = goalNode;
+        while (buffer != null && buffer.GetPosition() != start) {
+            path.Add(buffer.GetPosition());
+            buffer = buffer.GetParent();
+        }
+
+        return path;
+    }
+
+    // DOUBLE IN RiverGeneration.cs
+    private static List<Vector2> GetNeighbours(int[,] map, Vector2 cell) {
+        List<Vector2> neighbours = new List<Vector2>();
+
+        if (cell.x - 1 >= 0) neighbours.Add(new Vector2(cell.x - 1, cell.y));
+        if (cell.x + 1 < map.GetLength(1)) neighbours.Add(new Vector2(cell.x + 1, cell.y));
+        if (cell.y - 1 >= 0) neighbours.Add(new Vector2(cell.x, cell.y - 1));
+        if (cell.y + 1 < map.GetLength(0)) neighbours.Add(new Vector2(cell.x, cell.y + 1));
+
+        return neighbours;
+    }
+
+    private static void RemoveFromList(List<AStarNode> list, AStarNode node) {
+        AStarNode toRemove = null;
+        foreach (AStarNode n in list) {
+            if (n.GetPosition() == node.GetPosition()) toRemove = n;
+        }
+        if (toRemove != null) list.Remove(toRemove);
+    }
+
+    private static AStarNode GetNodeFromList(List<AStarNode> list, Vector2 pos) {
+        foreach (AStarNode n in list) {
+            if (n.GetPosition() == pos) return n;
+        }
+        return null;
+    }
+
+    private static bool IsInList(List<AStarNode> list, Vector2 pos) {
+        foreach (AStarNode n in list) {
+            if (n.GetPosition() == pos) return true;
+        }
+        return false;
+    }
+
+    private static bool IsNodeInListWithLowerF(List<AStarNode> list, AStarNode node) {
+        foreach (AStarNode n in list) {
+            if (node.GetPosition() == n.GetPosition() && node.GetF() > n.GetF()) return true;
+        }
+        return false;
+    }
+
+    private static AStarNode GetLowestF(List<AStarNode> nodes) {
+        float minF = 9999999999;
+        AStarNode minNode = nodes[0];
+        foreach (AStarNode node in nodes) {
+            if (node.GetF() < minF) {
+                minF = node.GetF();
+                minNode = node;
+            }
+        }
+        return minNode;
+    }
+
+    protected class AStarNode {
+        Vector2 pos;
+        float g;
+        float h;
+        AStarNode parent;
+
+        public AStarNode(AStarNode parent, Vector2 pos, Vector2 goal, float g) {
+            this.parent = parent;
+            this.pos = pos;
+            this.g = g;
+            this.h = (Mathf.Abs(pos.x - goal.x) + Mathf.Abs(pos.y - goal.y)) * 0.5f;
+        }
+
+        public void SetG(float g) {
+            this.g = g;
+        }
+
+        public AStarNode GetParent() { return parent; }
+        public Vector2 GetPosition() { return pos; }
+        public float GetG() { return g; }
+        public float GetF() { return g + h; }
+    }
+}
--- Assets/Scripts/Map/Generation/RiverGeneration.cs
+++ Assets/Scripts/Map/Generation/RiverGeneration.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 // https://brilliant.org/wiki/a-star-search/
 // https://www.geeksforgeeks.org/a-search-algorithm/
-// https://www.educative.io/edpresso/what-is-the-a-star-algorithm
 // https://mat.uab.cat/~alseda/MasterOpt/AStar-Algorithm.pdf
 
 public static class RiverGeneration {
@@ -22,247 +21,17 @@ public static class RiverGeneration {
             int start = Random.Range(0, waterBodies.Count - 1);
             Vector2 end = GetNearestPoint(waterBodiesCenter, waterBodiesCenter[start]);
 
-            //DebugUtils.DumpString("River from point (" + waterBodiesCenter[start].x + ";" + waterBodiesCenter[start].y + ") to point (" + end.x + ";" + end.y + ")");
-
-            List<Vector2> path = AStarRiver(map, noise, waterBodiesCenter[start], end);
+            List<Vector2> path = AStar.GeneratePath(map, noise, waterBodiesCenter[start], end);
             foreach (Vector2 cell in path) {
                 waterCells.Add(cell);
             }
         }
-
-        /*for (int i = 0; i < waterBodiesCenter.Count - 1; i++) {
-            List<Vector2> path = AStarRiver(map, noise, waterBodiesCenter[i], GetNearestPoint(waterBodiesCenter, waterBodiesCenter[i]));
-            foreach (Vector2 cell in path) {
-                waterCells.Add(cell);
-            }
-        }*/
 
         return waterCells;
     }
 
-    private static List<Vector2> AStarRiver(int[,] map, float[,] noise, Vector2 start, Vector2 goal) {
-
-        AStarNode goalNode = null;
-
-        // Create open and closed lists
-        List<AStarNode> open = new List<AStarNode>();
-        List<AStarNode> closed = new List<AStarNode>();
-
-        // Put node_start in the open list
-        open.Add(new AStarNode(null, start, goal, 0));
-
-        // While the open list is not empty
-        while (open.Count > 0) {
-            // Take the node with the lowest f from the open list
-            AStarNode current = GetLowestF(open);
-            open.Remove(current);
-
-            DebugUtils.DumpString("current : " + current.GetPosition().ToString() + " / goal : " + goal.ToString());
-
-            // If the current node is the goal, we found the solution
-            if (current.GetPosition() == goal) {
-                goalNode = current;
-                break;
-            }
-
-            // We generate all neighbours from the current node
-            foreach (Vector2 neighbourPos in GetNeighbours(map, current.GetPosition())) {
-
-                DebugUtils.DumpString("checking neighbour : " + neighbourPos.ToString());
-
-                // We create the neighbour node and set its f
-                float cellNoise = noise[(int)neighbourPos.y, (int)neighbourPos.x];
-
-                float mapDivider;
-                switch (map[(int)neighbourPos.y, (int)neighbourPos.x]) {
-                    case 2:
-                        mapDivider = 0.0000000000000000001f;
-                        break;
-
-                    case 3:
-                        mapDivider = 100000000000000000000f;
-                        break;
-
-                    default:
-                        mapDivider = 1f;
-                        break;
-                }
-
-                float neighbourCost = current.GetG() + cellNoise / mapDivider;
-
-
-                if (IsInList(open, neighbourPos)) {
-                    AStarNode tmp = GetNodeFromList(open, neighbourPos);
-                    DebugUtils.DumpString("neighbour is in open list");
-                    if (tmp.GetG() <= neighbourCost) continue;
-                    DebugUtils.DumpString("with higer g value");
-                }
-                else if (IsInList(closed, neighbourPos)) {
-                    AStarNode tmp = GetNodeFromList(closed, neighbourPos);
-                    DebugUtils.DumpString("neighbour is in closed list");
-                    if (tmp.GetG() <= neighbourCost) continue;
-                    DebugUtils.DumpString("with higer g value, sending it to open list");
-                    open.Add(tmp);
-                    RemoveFromList(closed, tmp);
-                }
-                else {
-                    DebugUtils.DumpString("neighbour is in no lists, sending it to open list");
-                    AStarNode neighbourNode = new AStarNode(current, neighbourPos, goal, neighbourCost);
-                    open.Add(neighbourNode);
-                }
-            }
-
-            closed.Add(current);
-        }
-
-        List<Vector2> path = new List<Vector2>();
-        AStarNode buffer = goalNode;
-        while (buffer != null && buffer.GetPosition() != start) {
-            path.Add(buffer.GetPosition());
-            buffer = buffer.GetParent();
-        }
-
-        return path;
-    }
-
-    /* private static List<Vector2> AStarRiver(int[,] map, float[,] noise, Vector2 start, Vector2 goal) {
-        List<AStarNode> closed = new List<AStarNode>();
-
-        List<AStarNode> open = new List<AStarNode>();
-        open.Add(new AStarNode(null, start, start, 0));
-
-        AStarNode goalNode = null;
-
-        bool searching = true;
-        while (searching && open.Count > 0) {
-
-            // DEBUG
-            //DebugUtils.DumpString("open count : " + open.Count + " / closed count : " + closed.Count);
-
-            AStarNode q = GetLowestF(open);
-            open.Remove(q);
-
-            DebugUtils.DumpString("current : " + q.GetPosition().ToString() + " / goal : " + goal.ToString());
-
-            if (q.GetPosition() == goal) {
-                goalNode = q;
-                break;
-            }
-
-            foreach (Vector2 neighbour in GetNeighbours(map, q.GetPosition())) {
-                if (IsCellValid(map, neighbour)) {
-
-                    /*if (neighbour == goal) {
-                        goalNode = new AStarNode(q, neighbour, goal, q.GetG() + 0);
-                        searching = false;
-                        break; 
-                    }
-
-                    float mapDivider;
-                    switch (map[(int)neighbour.y, (int)neighbour.x]) {
-                        case 2:
-                            mapDivider = 0.01f;
-                            break;
-
-                        case 3:
-                            mapDivider = 1000f;
-                            break;
-
-                        default:
-                            mapDivider = 1f;
-                            break;
-                    }
-
-
-                    float cellNoise = noise[(int)neighbour.y, (int)neighbour.x];
-                    AStarNode neighbourNode = new AStarNode(q, neighbour, goal, q.GetG() + cellNoise / mapDivider);
-
-                    if (IsInList(open, neighbourNode)) {
-                        if (GetNodeFromList(open, neighbourNode).GetG() <= neighbourNode.GetG()) break;
-                    }
-                    else if (IsInList(closed, neighbourNode)) {
-                        if (GetNodeFromList(closed, neighbourNode).GetG() <= neighbourNode.GetG()) break;
-                        RemoveFromList(closed, neighbourNode);
-                        open.Add(neighbourNode);
-                    }
-                    else {
-                        open.Add(neighbourNode);
-                    }
-
-
-
-                    /*if (!closed.Contains(neighbourNode)) {
-                        if (!IsNodeInListWithLowerF(open, neighbourNode)) {
-                            open.Add(neighbourNode);
-                        }
-                    }*/
-
-                    /*if (!IsNodeInListWithLowerF(open, neighbourNode)) {
-                        if (!IsNodeInListWithLowerF(closed, neighbourNode)) {
-                            open.Add(neighbourNode);
-                        }
-                    }
-                }
-            }
-
-            closed.Add(q);
-        }
-        DebugUtils.DumpString("A* over");
-
-        List<Vector2> path = new List<Vector2>();
-        AStarNode buffer = goalNode;
-        while (buffer != null && buffer.GetPosition() != start) {
-            path.Add(buffer.GetPosition());
-            buffer = buffer.GetParent();
-        }
-
-        return path;
-    } */
-
-    private static void RemoveFromList(List<AStarNode> list, AStarNode node) {
-        AStarNode toRemove = null;
-        foreach (AStarNode n in list) {
-            if (n.GetPosition() == node.GetPosition()) toRemove = n;
-        }
-        if (toRemove != null) list.Remove(toRemove);
-    }
-
-    private static AStarNode GetNodeFromList(List<AStarNode> list, Vector2 pos) {
-        foreach (AStarNode n in list) {
-            if (n.GetPosition() == pos) return n;
-        }
-        return null;
-    }
-
-    private static bool IsInList(List<AStarNode> list, Vector2 pos) {
-        foreach (AStarNode n in list) {
-            if (n.GetPosition() == pos) return true;
-        }
-        return false;
-    }
-
-    private static bool IsNodeInListWithLowerF(List<AStarNode> list, AStarNode node) {
-        foreach (AStarNode n in list) {
-            if (node.GetPosition() == n.GetPosition() && node.GetF() > n.GetF()) return true;
-        }
-        return false;
-    }
-    
-    private static AStarNode GetLowestF(List<AStarNode> nodes) {
-        float minF = 9999999999;
-        AStarNode minNode = nodes[0];
-        foreach (AStarNode node in nodes) {
-            if (node.GetF() < minF) {
-                minF = node.GetF();
-                minNode = node;
-            }
-        }
-        return minNode;
-    }
-
     public static void Debug(int[,] map) {
         List<List<Vector2>> waterBodies = GetWaterBodies(map);
-        // DebugUtils.DumpString("Water bodies count : " + waterBodies.Count);
 
         List<string> a = new List<string>();
         int count = 0;
@@ -340,6 +109,7 @@ public static class RiverGeneration {
         return false;
     }
 
+    // DOUBLE IN AStar.cs
     private static List<Vector2> GetNeighbours(int[,] map, Vector2 cell) {
         List<Vector2> neighbours = new List<Vector2>();
 
@@ -366,29 +136,6 @@ public static class RiverGeneration {
 
     private static bool IsCellValid(int[,] map, Vector2 cell) {
         return (cell.x >= 0 && cell.x < map.GetLength(1) && cell.y >= 0 && cell.y < map.GetLength(0));
-    }
-
-    protected class AStarNode {
-        Vector2 pos;
-        float g;
-        float h;
-        AStarNode parent;
-
-        public AStarNode(AStarNode parent, Vector2 pos, Vector2 goal, float g) {
-            this.parent = parent;
-            this.pos = pos;
-            this.g = g;
-            this.h = (Mathf.Abs(pos.x - goal.x) + Mathf.Abs(pos.y - goal.y)) * 0.5f;
-        }
-
-        public void SetG(float g) {
-            this.g = g;
-        }
-
-        public AStarNode GetParent() { return parent; }
-        public Vector2 GetPosition() { return pos; }
-        public float GetG() { return g; }
-        public float GetF() { return g+h; }
     }
 }
 
